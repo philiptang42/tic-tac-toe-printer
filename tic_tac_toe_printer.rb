a7d7700ac6e0a0e4d7c4e3e4acfb99e18a182f74
philiptang42 <philiptang42@gmail.com>
2015-12-17 21:25:33 -0500

    solution

--- tic_tac_toe_printer.rb
+++ tic_tac_toe_printer.rb
@@ -1,20 +1,38 @@
 def print_grid(board)
   board.each_with_index do |row, row_index|
     row_line = ""
-    row.each_with.index do |occupied, occupied_index|
+    row.each_with_index do |occupied, occupied_index|
       space = occupied
       if occupied.nil?
         space = " "
       end
-      row_line += "#{occupied}"
+      row_line += " #{space} "
       if occupied_index != row.size - 1
         row_line += "|"
       end
     end
     puts row_line
 
-    if row_index != row_size - 1
+    if row_index != row.size - 1
       puts ('---' * row.size) + ('-' * (row.size - 1))
     end
   end
 end
+
+board_a = [
+  ['x', 'o', 'x'],
+  ['x', nil, 'o'],
+  ['x', 'o', nil]
+]
+
+board_b = [
+  [nil, 'o', 'x'],
+  ['x', 'o', nil],
+  ['x', 'o', nil]
+]
+
+print_grid(board_a)
+
+puts "\n \n"
+
+print_grid(board_b)
